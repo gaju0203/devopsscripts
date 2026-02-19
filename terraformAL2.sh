5d1f02d324a5acdb92a15f26b060df772ffd7634
GAJANAN SAWASE <shitroplayz@gmail.com>
2026-02-19 14:31:47 +0530

    Update terraformAL2.sh

--- terraformAL2.sh
+++ terraformAL2.sh
@@ -3,3 +3,12 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum install -y terraform
 terraform version
+
+---------------------
+
+apt update -y
+apt install awscli -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+terraform -v
